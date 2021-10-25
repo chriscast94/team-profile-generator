38ac3340e03dac69823392fbac7b0dfbdedc122b
Christian Castillo <chriscast94@outlook.com>
2021-10-24 18:22:17 -0700

    adjusted code and html

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-//Create new folder and html file with all the team members information; Source: https://arpadt.com/articles/path-join-vs-path-resolve-nodejs
-// const directory = path.resolve(__dirname, 'public');
-// const outputPath = path.join(directory, 'team-members.html');
-
 const employeesArray = [];
 
 // Ask questions regarding team members
@@ -19,7 +15,7 @@ const newTeamMember = () => {
             type: 'list',
             name: 'role',
             message: 'What is the employee role?',
-            choices: ['Intern', 'Engineer', 'Manager']
+            choices: ['Manager', 'Engineer', 'Intern']
         },
 
         {
@@ -95,16 +91,97 @@ const newTeamMember = () => {
             }
 
         });
+};
 
+function generateHTML() {
+    let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Team Generator</title>
+</head>
 
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="team-cards row">
+            <div class="card">
+                <div class="card-body">`
+
+    employeesArray.manager.foreach((managersArrayElements) => {
+        html += `
+                    <div class="container">
+    <div class="team-cards row">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">${managersArrayElements.grabName()}</h4>
+                <h5 class="card-subtitle mb-2 text-muted">${managersArrayElements.grabRole()}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${managersArrayElements.grabId()}</li>
+                    <li class="list-group-item">Email: ${managersArrayElements.grabEmail()}</li>
+                    <li class="list-group-item">Office Number: ${managersArrayElements.grabOffice()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>`;
+    })
+    employeesArray.engineer.foreach((engineersArrayElements) => {
+        html += `
+        <div class="container">
+    <div class="team-cards row">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">${engineersArrayElements.grabName()}</h4>
+                <h5 class="card-subtitle mb-2 text-muted">${engineersArrayElements.grabRole()}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${engineersArrayElements.grabId()}</li>
+                    <li class="list-group-item">Email: ${engineersArrayElements.grabEmail()}</li>
+                    <li class="list-group-item">Github: ${engineersArrayElements.grabGithub()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>`;
+    })
+
+    employeesArray.intern.foreach((internsArrayElements) => {
+        html += `
+        <div class="container">
+    <div class="team-cards row">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">${internsArrayElements.grabName()}</h4>
+                <h5 class="card-subtitle mb-2 text-muted">${internsArrayElements.grabRole()}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${internsArrayElements.grabId()}</li>
+                    <li class="list-group-item">Email: ${internsArrayElements.grabEmail()}</li>
+                    <li class="list-group-item">School: ${internsArrayElements.grabSchool()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>`
+    })
 }
 
-
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((data) => {
-        writeToFile("index.html", markdown(data));
-        // then (() => console.log("Congratulations!\nSuccessfully created and wrote new README.md file."))
-    })
+const init = () => {
+    newTeamMember()
+        // Use writeFileSync method to use promises instead of a callback function
+        .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+        .then(() => console.log('Successfully wrote to index.html'))
+        .catch((err) => console.error(err));
 };
+
+init();
